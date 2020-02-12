2586e15fe0f08eaf9180cea62da5f976fbfca165
GunavaranBrihadiswaran <150196b@uom.lk>
2020-02-12 11:21:54 +0530

    SearchResult updated

--- src/ds/core/SearchResult.java
+++ src/ds/core/SearchResult.java
@@ -3,16 +3,14 @@ package ds.core;
 public class SearchResult {
     private String fileName;
     private String address;
-    private int port;
     private int tcpPort;
     private int hops;
 //    private long timeElapsed;
 
 
-    public SearchResult(String fileName, String address, int port, int tcpPort, int hops) {
+    public SearchResult(String fileName, String address, int tcpPort, int hops) {
         this.fileName = fileName;
         this.address = address;
-        this.port = port;
         this.tcpPort = tcpPort;
         this.hops = hops;
     }
@@ -23,10 +21,6 @@ public class SearchResult {
 
     public String getAddress() {
         return address;
-    }
-
-    public int getPort() {
-        return port;
     }
 
     public int getTcpPort() {
